e765968a6c2e01dcc5fb54775e122c8cd2cfec66
mfando <fando97@mail.ru>
2021-02-08 02:11:22 +0300

    Calc fibonacci

--- lesson-5/main.go
+++ lesson-5/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+//todo переписать на хэш таблицы
+func fibonacci(n uint) uint {
+	if n < 2 {
+		return n
+	}
+
+	return fibonacci(n-1) + fibonacci(n-2)
+}
+func main() {
+	// todo добавить ввод с консоли
+	fmt.Println(fibonacci(10))
 }
