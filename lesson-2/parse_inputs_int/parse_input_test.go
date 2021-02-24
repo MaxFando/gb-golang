ede7689b6ce8e8b43a40068170ae88f3d19e848c
mfando <fando97@mail.ru>
2021-02-24 11:39:17 +0300

    add tests: parse_input test table

--- lesson-2/parse_inputs_int/parse_input_test.go
+++ lesson-2/parse_inputs_int/parse_input_test.go
@@ -12,8 +12,18 @@ func ExampleParse() {
 }
 
 func TestParse(t *testing.T) {
-	want := "В числе 234 - 2 сотен, 3 десятков, 4 единиц"
-	if got, _ := Parse(234); got != want {
-		t.Errorf("Parse(234) = %s, want %s", got, want)
+	tests := []struct {
+		num  uint32
+		want string
+	}{
+		{num: 234, want: "В числе 234 - 2 сотен, 3 десятков, 4 единиц"},
+		{num: 582, want: "В числе 582 - 5 сотен, 8 десятков, 2 единиц"},
+		{num: 102, want: "В числе 102 - 1 сотен, 0 десятков, 2 единиц"},
+	}
+
+	for _, tc := range tests {
+		if got, _ := Parse(tc.num); got != tc.want {
+			t.Errorf("Parse(234) = %s, want %s", got, tc.want)
+		}
 	}
 }
